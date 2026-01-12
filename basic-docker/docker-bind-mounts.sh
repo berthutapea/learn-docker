92b85fe5408db05674ba5d6b3bd6faf44af5301a
Gilbert Hutapea <berthutapea@gmail.com>
2026-01-12 20:33:12 +0700

    chore: add docker bind mounts

---
+++ basic-docker/docker-bind-mounts.sh
@@ -0,0 +1,1 @@
+docker container create --name mongodata --publish 27018:27017 --mount "type=bind,source=C:\Users\amaro\mongo-data,destination=/data/db" --env MONGO_INITDB_ROOT_USERNAME=gilbert --env MONGO_INITDB_ROOT_PASSWORD=gilbert mongo:latest
