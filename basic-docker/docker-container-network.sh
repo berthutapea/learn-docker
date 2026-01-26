6e190d45129e1edeeae822733860b0cd3e4e2926
Gilbert Hutapea <berthutapea@gmail.com>
2026-01-26 20:51:44 +0700

    chore: add container network

---
+++ basic-docker/docker-container-network.sh
@@ -0,0 +1,17 @@
+docker network create --driver bridge mongonetwork
+
+docker container create --name mongodb --network mongonetwork --env MONGO_INITDB_ROOT_USERNAME=gilbert --env MONGO_INITDB_ROOT_PASSWORD=gilbert mongo:latest
+
+docker image pull mongo-express:latest
+
+docker container create --name mongodbexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://gilbert:gilbert@mongodb:27017/" mongo-express:latest
+
+docker container start mongodb
+
+docker container start mongodbexpress
+
+docker network disconnect mongonetwork mongodb
+
+docker container logs mongodbexpress
+
+docker network connect mongonetwork mongodb
