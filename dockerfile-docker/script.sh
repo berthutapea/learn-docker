216320d913e9859843e76f6799ba50a505f1bfd0
Gilbert Hutapea <berthutapea@gmail.com>
2026-02-05 19:31:38 +0700

    chore: add run instruction

--- dockerfile-docker/script.sh
+++ dockerfile-docker/script.sh
@@ -1,3 +1,15 @@
+# From Instruction
 docker build -t berthutapea/from from
 
 docker image ls
+
+# RUN Instruction
+docker build -t berthutapea/run run
+
+docker image rm berthutapea/run
+
+docker build -t berthutapea/run run --progress=plain --no-cache
+
+docker image ls | grep berthutapea
+
+docker image ls | findstr berthutapea
